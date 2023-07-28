6cbe6431e3570ee37ab529b44ad79e5c75890abf
regiskeller1 <regiskeller17@gmail.com>
2023-07-28 19:13:43 -0300

    entendendo operadores lógicos

--- src/Operadores.java
+++ src/Operadores.java
@@ -2,12 +2,19 @@ public class Operadores {
 
     public static void main(String[] args) {
 
-       int numero1 = 1;
-       int numero2 = 2;
+       boolean condicao1 = true;
+       boolean condicao2 = false;
 
-       if (numero1 != numero2){
-           System.out.println("numero 1 é diferente de numero 2");
+       if(condicao1 && condicao2) {
+           System.out.println("as duas condições são iguais");
+       } else{
+           System.out.println("as duas condições são diferentes");
        }
 
+       if(condicao1 || condicao2) {
+           System.out.println("uma das condições são verdadeiras");
+       } else {
+           System.out.println("nenhuma das condições são verdadeiras");
+       }
     }
 }
