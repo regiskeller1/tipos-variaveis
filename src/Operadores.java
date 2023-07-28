70329e92a1ae42d5354b22e18046e12715e6ba71
AndersonLandgraf <landgraf_anderson@hotmail.com>
2023-07-28 19:29:09 -0300

    Commitei minha feature

--- src/Operadores.java
+++ src/Operadores.java
@@ -4,6 +4,7 @@ public class Operadores {
 
        boolean condicao1 = true;
        boolean condicao2 = false;
+       boolean condicaoMerge = false;
 
        if(condicao1 && condicao2) {
            System.out.println("as duas condições são iguais");
@@ -11,7 +12,7 @@ public class Operadores {
            System.out.println("as duas condições são diferentes");
        }
 
-       if(condicao1 || condicao2) {
+       if(condicao1 || condicaoMerge || condicao2) {
            System.out.println("uma das condições são verdadeiras");
        } else {
            System.out.println("nenhuma das condições são verdadeiras");
