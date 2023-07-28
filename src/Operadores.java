87b657ff96b08552f91a57e878bcbc36fddebfbd
regiskeller1 <regiskeller17@gmail.com>
2023-07-28 19:01:05 -0300

    Entendendo operadores relacionais

--- src/Operadores.java
+++ src/Operadores.java
@@ -2,13 +2,12 @@ public class Operadores {
 
     public static void main(String[] args) {
 
-        int a, b;
-        a = 3;
-        b = 4;
+       int numero1 = 1;
+       int numero2 = 2;
 
-        String resultado = a == b ? "verdadeiro" : "falso";
+        System.out.println(numero1 == numero2);
 
-        System.out.println(resultado);
+        System.out.println(numero1 < numero2);
 
     }
 }
