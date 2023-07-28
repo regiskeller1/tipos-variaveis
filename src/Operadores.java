89e5799cc45d4634337c7a132147bde324476cf4
AndersonLandgraf <landgraf_anderson@hotmail.com>
2023-07-28 19:53:36 -0300

    Removendo meu merge

--- src/Operadores.java
+++ src/Operadores.java
@@ -5,7 +5,6 @@ public class Operadores {
        boolean condicao1 = true;
        boolean condicao2 = false;
        boolean condicaoRegis = true;
-       boolean condicaoMerge = false;
 
        if(condicao1 && condicao2) {
            System.out.println("as duas condições são iguais");
@@ -13,7 +12,7 @@ public class Operadores {
            System.out.println("as duas condições são diferentes");
        }
 
-       if(condicao1 || condicaoMerge || condicaoRegis || condicao2) {
+       if(condicao1 || condicaoRegis || condicao2) {
            System.out.println("uma das condições são verdadeiras");
        } else {
            System.out.println("nenhuma das condições são verdadeiras");
